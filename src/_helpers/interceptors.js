fedd844d3bbf75088667e45232d015ea546dac96
Kiril Daskalov <webmatrixxxl@gmail.com>
2019-09-05 00:34:19 +0300

    Add: 401 unauthenticated redirect to "/logout" - add endpoints in config

--- src/_helpers/interceptors.js
+++ src/_helpers/interceptors.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { history } from '../_helpers';
 import config from '../config/config';
 
 const axiosInstance = axios.create({
@@ -7,23 +7,48 @@ const axiosInstance = axios.create({
 })
 
 const isHandlerEnabled = (config={}) => {
-  return config.hasOwnProperty('handlerEnabled') && !config.handlerEnabled ?
+  return config.hasOwnProperty('preventInterception') && config.preventInterception ?
     false : true
 }
 
 const requestHandler = (request) => {
-  console.log('requestHandlerrequestHandlerrequestHandlerrequestHandlerrequestHandler');
+  // console.log(request);
 
-  // if (isHandlerEnabled(request)) {
+  if (isHandlerEnabled(request)) {
     // Modify request here
     request.headers['Authorization'] = 'Bearer ' + localStorage.getItem('token')
-  // }
+  }
   return request
 }
 
+const errorHandler = (error) => {
+  console.log(error);
+
+  if (isHandlerEnabled(error.config)) {
+    console.log(error);
+  }
+  return Promise.reject({ ...error })
+}
+
+const successHandler = (response) => {
+  if (isHandlerEnabled(response.config)) {
+    // Handle responses
+  }
+  return response
+}
+
 axios.interceptors.request.use(
-  request => requestHandler(request)
+  request => requestHandler(request),
 )
 
+axios.interceptors.response.use(response => response, error => {
+  const status = error.response ? error.response.status : null
 
-console.log('AOOPOPOPSOPSOPSO')
+  if (status === 401) {
+    history.push(config.appUrls.logout);
+  }
+
+  return Promise.reject(error);
+});
+
+
--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,36 @@
 module.exports = {
-  baseUrl: 'http://localhost:8000/api/'
+  baseUrl: 'http://localhost:8000/api/',
+  appUrls: {
+    logout: {
+      title: 'Logout',
+      url: '/logout',
+    },
+  },
+  apiUrls: {
+    geo: {
+      url: 'geo',
+      result: {
+        url: '/result'
+      },
+      q: {
+        url: '/q'
+      },
+      clear: {
+        url: '/clear'
+      },
+    },
+    collections: {
+      url: '/collections',
+      add: {
+        url: '/add'
+      },
+      info: {
+        url: '/info'
+      },
+      new: {
+        url: '/new'
+      }
+    }
+  }
 }
+
